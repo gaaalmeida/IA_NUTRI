243cb1a3ef1d75952665d0da3ca0784e4bb09315
Gabriel Almeida <gaaalmeidasan@gmail.com>
2021-11-05 18:07:35 -0300

    Update genetic.py

--- genetic.py
+++ genetic.py
@@ -3,221 +3,199 @@ Carlos Gabriel
 Isaque Almeida
 Luis Fernando
 """
-from typing import List, Callable, Tuple
-from collections import namedtuple
 import random
-from functools import partial
+from time import time
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Quantidade de calorias desejada por dia
-max_calories = 2500
+CALORIES = 2500
 
-"""
-    Genome -- Cria um individuo
-    Population -- Conjunto de individuos
-    FitnessFunction -- Calcula o 'score' de um individuo
-    PopulateFunction -- Gera aleatoriamente a população
-    SelectionFunc -- Seleciona dois individuos em uma população
-    CrossoverFunc -- Gera os filhos de dois individuos
-    MutationFunc -- Gera uma mutaçao aleatoria em algum individuo
-"""
-Genome = List[int]
-Population = List[Genome]
-FitnessFunc = Callable[[Genome], int]
-PopulateFunc = Callable[[], Population]
-SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Genome, Genome]]
-CrossoverFunc = Callable[[Genome, Genome], Tuple[Genome, Genome]]
-MutationFunc = Callable[[Genome], Genome]
-Food = namedtuple('Food', ['name', 'calories'])
+foods = (
+    [('Pão Integral 100g', 246),
+    ('Cereal Flocos de Milho 100g', 357),
+    ('Salada Verde com Abacate 250g', 135),
+    ('Batata Frita 200g', 321),
+    ('Creme de Papaia 150g', 177),
+    ('Big Mac', 740),
+    ('Leite Semi-Desnatado 100g', 42),
+    ('Frango grelhado 200g', 318),
+    ('Torta de Legumes 250g', 224),
+    ('Ovo Cozido 2un', 143),
+    ('Queijo Branco 100g', 345),
+    ('Coca-Cola 300ml', 313),
+    ('Vitamina de Banana 250g', 288),
+    ('Arroz Integral 200g', 222),
+    ('Uva-passa 100g', 299),
+    ('Abacaxi 100g', 50),
+    ('Sorvete 100g', 207),
+    ('Lasanha 100g', 135),
+    ('Feijoada 100g', 305),
+    ('Estrogonofe 100g', 98),
+    ('Sopa de Vegetais 500g', 159)]
+)
 
-foods = [
-    Food('Double Quarter', 742),
-    Food('Pão Integral 100g', 246),
-    Food('Cereal Flocos de Milho 100g', 357),
-    Food('Salada Verde com Abacate 250g', 135),
-    Food('Batata Frita 200g', 321),
-    Food('Creme de Papaia 150g', 177),
-    Food('Leite Semi-Desnatado 100g', 42),
-    Food('Frango grelhado 200g', 318),
-    Food('Torta de Legumes 250g', 224),
-    Food('Ovo Cozido 2un', 143),
-    Food('Queijo Branco 100g', 345),
-    Food('Coca-Cola 300ml', 313),
-    Food('Vitamina de Banana 250g', 288),
-    Food('Arroz Integral 200g', 222),
-    Food('Uva-passa 100g', 299),
-    Food('Abacaxi 100g', 50),
-    Food('Sopa de Vegetais 500g', 159)
-]
-
-
-def generate_genome(length: int) -> Genome:
-    """Gera um individuo
-
-    Keyword arguments:
-    length -- tamanho do individuo a ser gerado
-    """
+def generate_chromosome(length):
     return random.choices([0,1], k=length)
 
+def generate_population(size, chromosome_length):
+    return ([generate_chromosome(chromosome_length) for _ in range(size)])
 
-def generate_population(size: int, genome_length: int) -> Population:
-    """
-    size: Quantidade de soluções a ser gerada
-    genome_length: Tamanho de cada solução
-    """
-    return [generate_genome(genome_length) for _ in range(size)]
+def gen_pop_w_limit(size, chromosome_length, minimum_distance):
+    pop = generate_population(size, chromosome_length)
+    pop_s = full_population_fitness(pop)
+    print(pop_s)
 
+    while True:
+        for i, score in enumerate(pop_s):
+            if score < minimum_distance:
+                pop[i] = generate_chromosome(len(foods))
+                pop_s = full_population_fitness(pop)
+        
+        pop_s = full_population_fitness(pop)
+        x = 0
+        for i, score in enumerate(pop_s):
+            if score < minimum_distance:
+                x += 1
+        if x == 0:
+            break
 
-def fitness(genome: Genome, foods: List[Food], calories_limit: int) -> int:
-    """Calcula a pontuação de cada individuo
-
-    Keyword arguments:
-    genome -- individuo de uma população
-    foods -- lista de alimentos
-    calories_limit -- limite de calorias
-    """
-    if len(genome) != len(foods):
-        raise ValueError("Genome and Food must be of the same length!")
-
-    total_cal = 0
-
-    for i, food in enumerate(foods):
-        if genome[i] == 1:
-            total_cal += food.calories
-
-            if total_cal > calories_limit:
-                return 0
-
-    return total_cal
-
-
-def selection_pair(population: Population, fitness_func: FitnessFunc) -> Population:
-    """Seleciona dois individuos de uma população
+    pop_s = full_population_fitness(pop)
     
-    Keyword arguments:
-    population -- população onde os individuos serão escolhidos
-    fitness_func -- função de fitness, usada para calcular a pontuação de cada
-    individuo para definir quem será escolhido (a escolha é aleatoria, porém quem
-    possuir maior pontuação terá maior chance de ser escolhido)
+    return pop
+
+
+def fitness(chromosome):
+    """Calcula a pontuação de um cromossomo baseado na distancia que ele se
+       encontra da resposta esperada.
     """
-    return random.choices(
-        population=population,
-        weights=[fitness_func(genome) for genome in population],
-        k=2
-    )
+    chromosome_value = sum([foods[x][1] for x, y in enumerate(chromosome) if y > 0])
 
+    score = abs(CALORIES - chromosome_value)
 
-def single_point_crossover(a: Genome, b: Genome) -> Tuple[Genome, Genome]:
-    """Gera filhos de dois individuos
+    return score
 
-    Keyword arguments:
-    a -- individuo 1
-    b -- individuo 2
-    """
+def full_population_fitness(population):
+    """Retorna um array com a pontuação da população inteira na mesma ordem."""
+    return ([fitness(chromosome) for chromosome in population])
+
+def selection_pair(population, pop_scores):
+    """Cria uma roleta para escolher aleatoriamente dois individuos da população"""
+    while True:
+        sp = random.choices(
+            population=population,
+            k=2,
+            weights=pop_scores)
+
+        if sp[0] != sp[1]:
+            return sp
+        else:
+            continue 
+
+def single_point_crossover(a, b):
     if len(a) != len(b):
-        raise ValueError("a and b must be of the same length!")
-    
+        raise ValueError("Genome 'a' and 'b' must be of the same length!")
+
     length = len(a)
     if length < 2:
         return a, b
 
     p = random.randint(1, length-1)
-    return a[0:p] + b[p:], b[0:p] + a[p:]
+
+    offspring_a = a[:p] + b[p:]
+    offspring_b = b[:p] + a[p:]
+
+    return offspring_a, offspring_b
+
+def mutation(chromosome, probability = 0.05):
+    r = np.random.random_sample(size=None)
+
+    if r <= probability:
+        index = np.random.randint(low=0,high=(len(chromosome) - 1))
+        chromosome[index] = abs(chromosome[index] - 1)
+
+    return chromosome
+
+def run(generation_limit = 100, population_size = 100):
+    worst = []
+    average  = []
+    best  = []
+
+    foods_len = len(foods)
+    pop = (gen_pop_w_limit(population_size, foods_len, 1500))
 
 
-def mutation(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome:
-    """Gera uma mutação aleatoria em um individuo
-    
-    Keyword arguments:
-    genome -- individuo a ser mutado
-    num -- numero de vezes que a mutação ira ocorrer
-    probability -- probabilidade da mutação ocorrer em %
-    """
-    for _ in range(num):
-        index = random.randrange(len(genome))
-        genome[index] = genome[index] if random.random() > probability else abs(genome[index] - 1)
+    for g in range(generation_limit):
+        sorted_pop = sorted(pop, key=lambda chromosome: fitness(chromosome))
+        fpf = full_population_fitness(sorted_pop)
+        print(sorted_pop)
 
-    return genome
+        worst.append(fitness( sorted_pop[-1]))
+        average.append(fitness( sorted_pop[ len(sorted_pop) // 2 ] ))
+        best.append(fitness( sorted_pop[0] ))
 
-
-def run_evolution(
-    populate_func: PopulateFunc,
-    fitness_func: FitnessFunc,
-    fitness_limit: int,
-    selection_func: SelectionFunc = selection_pair,
-    crossover_func: CrossoverFunc = single_point_crossover,
-    mutation_func: MutationFunc = mutation,
-    generation_limit: int = 100
-) -> Tuple[Population, int]:
-    """Inicia o processo de evolução
-
-    Keyword arguments:
-    populate_func -- função que gera a população
-    fitness_func -- calcula a pontuação de um individuo
-    fitness_limit -- pontuação maxima esperada de um individuo
-    selection_func -- seleciona dois individuos de uma população
-    crossover_func -- cria dois filhos de uma população
-    mutation_func -- gera uma mutação em um individuo
-    generation_limit -- limite de gerações
-    """
-    population = populate_func()
-
-    for i in range(generation_limit):
-        population = sorted(
-            population,
-            key=lambda genome: fitness_func(genome),
-            reverse=True
-        )
-
-        if fitness_func(population[0]) == fitness_limit:
+        if best[len(best) - 1] < 1:
             break
 
-        next_generation = population[0:4]
+        new_generation = sorted_pop[:2]
+        off_a, off_b = single_point_crossover(new_generation[0], new_generation[1])
+        off_a = mutation(off_a)
+        off_b = mutation(off_b)
 
-        for _ in range(int(len(population) / 2) - 1):
-            parents = selection_func(population, fitness_func)
-            offspring_a, offspring_b = crossover_func(parents[0], parents[1])
-            offspring_a = mutation_func(offspring_a)
-            offspring_b = mutation_func(offspring_b)
-            next_generation += [offspring_a, offspring_b]
+        print(new_generation)
+        print(type(new_generation))
+        print(type(new_generation[0]))
 
-        population = next_generation
-    
-    population = sorted(
-        population,
-        key=lambda genome: fitness_func(genome),
-        reverse=True
-    )
+        new_generation.append(off_a)
+        new_generation.append(off_b)
+        print(f"New Generation: {new_generation}")
 
-    return population, i
+        for _ in range( int(population_size / 2) - 1 ):
+            parents = selection_pair(pop, fpf)
+            off_a, off_b = single_point_crossover(parents[0], parents[1])
+            off_a = mutation(off_a)
+            off_b = mutation(off_b)
+            new_generation.append(off_a)
+            new_generation.append(off_b)
 
+        pop = new_generation
 
-population, generations = run_evolution(
-    populate_func=partial(
-        generate_population, size=14, genome_length=len(foods)
-    ),
-    fitness_func=partial(
-        fitness, foods=foods, calories_limit=max_calories
-    ),
-    fitness_limit=2500,
-    generation_limit=1000
-)
+        print(f"WORST: {fitness( sorted_pop[-1] )}")
+        print(f"AVERAGE: {fitness( sorted_pop[ len(sorted_pop) // 2 ] )}")
+        print(f"BEST: {fitness( sorted_pop[0] )}")
 
+    return g, sorted_pop, best, average, worst
 
-def genome_to_foods(genome: Genome, foods: List[Food]) -> List[Food]:
-    """Função auxiliar para converter os resultados finais nas
-    subsequentes comidas com base em seu id.
-    """
+def chromosome_to_foods(chromosome):
     result = []
     total = 0
-    for i, gene in enumerate(genome):
+    for i, gene in enumerate(chromosome):
         if gene == 1:
-            result.append(foods[i].name)
-            total += foods[i].calories
+            result.append(foods[i][0])
+            total += foods[i][1]
 
     return result, total
 
+s = time()
+g, pop, best, average, worst = run(generation_limit=40000, population_size=len(foods)*2)
+e = time()
 
-print(f"Number of generations: {generations}")
-results = genome_to_foods(population[0], foods)
-print(f"Best solution: {results[0]}")
-print(f"Calories: {results[1]}")
+print(f"GENERATIONS: {g}")
+print(f"BEST: {best}")
+print(f"AVERAGE: {average}")
+print(f"WORST: {worst}")
+print(full_population_fitness(pop))
+
+result, total = chromosome_to_foods(pop[0])
+print(result)
+print(f"Total de calorias: {total}")
+
+generations_arr = [x for x in range(g + 1)]
+
+plt.plot(generations_arr, best, label="best")
+plt.plot(generations_arr, average, label="average")
+plt.plot(generations_arr, worst, label="worst")
+plt.ylabel("Distance to 2500kcal\n(lower is better)")
+plt.xlabel("Number of generations")
+plt.legend()
+plt.show()
+print(f"Elapsed: {e-s}")
